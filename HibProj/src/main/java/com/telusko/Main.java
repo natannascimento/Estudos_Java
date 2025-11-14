c1646d5ff573ae2306c84e40d01680c7255cf59a
Natan Nascimento <natannascimento@outlook.com>
2025-11-14 16:09:54 -0300

    Estudando Hibernate
    Fazendo consulta utilizando HQL (Hibernate Query Language)

--- HibProj/src/main/java/com/telusko/Main.java
+++ HibProj/src/main/java/com/telusko/Main.java
@@ -4,71 +4,29 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     static void main(String[] args)  {
 
-        Laptop l1 = new Laptop();
-        l1.setLid(1);
-        l1.setBrand("Asus");
-        l1.setModel("ROG");
-        l1.setRam(16);
-
-        Laptop l2 = new Laptop();
-        l2.setLid(2);
-        l2.setBrand("Dell");
-        l2.setModel("XPS");
-        l2.setRam(32);
-
-        Laptop l3 = new Laptop();
-        l3.setLid(3);
-        l3.setBrand("Apple");
-        l3.setModel("Mac");
-        l3.setRam(8);
-
-        Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setAname("Natan");
-        a1.setTech("Java");
-
-
-        Alien a2 = new Alien();
-        a2.setAid(102);
-        a2.setAname("Bruno");
-        a2.setTech("C#");
-
-        a1.setLaptops(Arrays.asList(l1,l2));
-        a2.setLaptops(Arrays.asList(l3));
-
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(com.telusko.Alien.class)
                 .addAnnotatedClass(com.telusko.Laptop.class)
                 .configure()
                 .buildSessionFactory();
 
         Session session = sf.openSession();
 
-        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("from Laptop where ram=32", Laptop.class);
+        List<Laptop> laptopList =  query.getResultList();
 
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
+        //Laptop l1 = session.find(Laptop.class, 3);
 
-        session.persist(a1);
-        session.persist(a2);
-
-        transaction.commit();
+        System.out.println(laptopList);
 
         session.close();
-
-        Session session1 = sf.openSession();
-
-        Alien a5 = session1.find(Alien.class, 101);
-        //System.out.println(a5);
-
-        session1.close();
 
         sf.close();
 
