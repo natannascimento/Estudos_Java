0fcecb807db8cb74b2d6f37f6dae97b9223d13e5
Natan Nascimento <natannascimento@outlook.com>
2025-11-18 18:09:25 -0300

    Scopes Update Alien bean configuration and add age property;

--- Spring1/src/main/java/com/telusko/App.java
+++ Spring1/src/main/java/com/telusko/App.java
@@ -9,9 +9,13 @@ public class App
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Alien alien1 = (Alien) context.getBean("alien");
-        alien1.code();
+        //alien1.code();
+        alien1.age = 21;
+
+        System.out.println(alien1.age);
 
         Alien alien2 = (Alien) context.getBean("alien");
-        alien2.code();
+        //alien2.code();
+        System.out.println(alien2.age);
     }
 }
