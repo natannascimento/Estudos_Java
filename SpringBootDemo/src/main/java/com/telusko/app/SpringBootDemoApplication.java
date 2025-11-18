93a4b8d1a774e964ac1ec70b76601bc9c9b9f2c0
Natan Nascimento <natannascimento@outlook.com>
2025-11-18 15:37:39 -0300

    Initialize Spring Boot application with basic structure and Alien component

---
+++ SpringBootDemo/src/main/java/com/telusko/app/SpringBootDemoApplication.java
@@ -0,0 +1,20 @@
+package com.telusko.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+
+        Alien alien = context.getBean(Alien.class);
+        alien.code();
+
+        Alien alien1 = context.getBean(Alien.class);
+        alien1.code();
+	}
+
+}
