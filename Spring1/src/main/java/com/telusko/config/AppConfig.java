e55fa422214492664e6d350bca8cd49e868baf06
Natan Nascimento <natannascimento@outlook.com>
2025-11-21 17:24:10 -0300

    Working with java based configuration Primary and Qualifier

--- Spring1/src/main/java/com/telusko/config/AppConfig.java
+++ Spring1/src/main/java/com/telusko/config/AppConfig.java
@@ -3,16 +3,19 @@ package com.telusko.config;
 import com.telusko.Alien;
 import com.telusko.Computer;
 import com.telusko.Desktop;
+import com.telusko.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public Alien alien(@Autowired Computer computer) {
+    public Alien alien(Computer computer) { //@Qualifier("desktop")
         Alien object = new Alien();
         object.setAge(21);
         object.setComputer(computer);
@@ -24,5 +27,11 @@ public class AppConfig {
     //@Scope("prototype")
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
