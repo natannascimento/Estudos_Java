a2dfc6a4d634badee5659549cad54480c9749450
Natan Nascimento <natannascimento@outlook.com>
2025-11-21 17:45:02 -0300

    Working with java based configuration Scope and Value annotation

--- Spring1/src/main/java/com/telusko/Alien.java
+++ Spring1/src/main/java/com/telusko/Alien.java
@@ -2,12 +2,14 @@ package com.telusko;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.beans.ConstructorProperties;
 @Component
 public class Alien {
 
+    @Value("21")
     private int age;
     private Computer computer;
 
