4ddf3fcde72ed846052b2ded6fc60e1c120289c9
Natan Nascimento <natannascimento@outlook.com>
2025-11-21 17:30:32 -0300

    Working with java based configuration Component Stereotype Annotation

--- Spring1/src/main/java/com/telusko/Alien.java
+++ Spring1/src/main/java/com/telusko/Alien.java
@@ -1,7 +1,9 @@
 package com.telusko;
 
-import java.beans.ConstructorProperties;
+import org.springframework.stereotype.Component;
 
+import java.beans.ConstructorProperties;
+@Component
 public class Alien {
 
     private int age;
--- Spring1/src/main/java/com/telusko/config/AppConfig.java
+++ Spring1/src/main/java/com/telusko/config/AppConfig.java
@@ -6,32 +6,31 @@ import com.telusko.Desktop;
 import com.telusko.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@ComponentScan("com.telusko")
 public class AppConfig {
 
-    @Bean
-    public Alien alien(Computer computer) { //@Qualifier("desktop")
-        Alien object = new Alien();
-        object.setAge(21);
-        object.setComputer(computer);
-        return object;
-    }
-
-//    @Bean(name = {"desktop1", "desktop2", "desktop3"})
-    @Bean
-    //@Scope("prototype")
-    public Desktop desktop() {
-        return new Desktop();
-    }
-
-    @Bean
-    @Primary
-    public Laptop laptop() {
-        return new Laptop();
-    }
+//    @Bean
+//    public Alien alien(Computer computer) { //@Qualifier("desktop")
+//        Alien object = new Alien();
+//        object.setAge(21);
+//        object.setComputer(computer);
+//        return object;
+//    }
+//
+////    @Bean(name = {"desktop1", "desktop2", "desktop3"})
+//    @Bean
+//    //@Scope("prototype")
+//    public Desktop desktop() {
+//        return new Desktop();
+//    }
+//
+//    @Bean
+//    @Primary
+//    public Laptop laptop() {
+//        return new Laptop();
+//    }
 }
