c3dba61f36d0901adf3dbebf44725b58c884f586
Natan Nascimento <natannascimento@outlook.com>
2025-11-21 16:23:01 -0300

    Working with java based configuration Using Bean name

--- Spring1/src/main/java/com/telusko/App.java
+++ Spring1/src/main/java/com/telusko/App.java
@@ -11,7 +11,7 @@ public class App
     {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Desktop desktop = context.getBean(Desktop.class);
+        Desktop desktop = context.getBean("desktop",Desktop.class);
         desktop.compile();
 
 //        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
--- Spring1/src/main/java/com/telusko/config/AppConfig.java
+++ Spring1/src/main/java/com/telusko/config/AppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+//    @Bean(name = {"desktop1", "desktop2", "desktop3"})
     @Bean
     public Desktop desktop() {
         return new Desktop();
