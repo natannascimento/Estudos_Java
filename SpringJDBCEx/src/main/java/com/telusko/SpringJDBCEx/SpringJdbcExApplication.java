bd4ebd2f8aad06c90c9ab973d820175ba1216887
Natan Nascimento <natannascimento@outlook.com>
2025-11-24 11:58:03 -0300

    Spring JDBC - Initialize Spring JDBC project with Maven setup and basic application structure

---
+++ SpringJDBCEx/src/main/java/com/telusko/SpringJDBCEx/SpringJdbcExApplication.java
@@ -0,0 +1,30 @@
+package com.telusko.SpringJDBCEx;
+
+import com.telusko.SpringJDBCEx.model.Student;
+import com.telusko.SpringJDBCEx.service.StudentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringJdbcExApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringJdbcExApplication.class, args);
+
+        Student student = context.getBean(Student.class);
+        student.setRollNo(1);
+        student.setName("Natan");
+        student.setMarks(95);
+
+        StudentService service = context.getBean(StudentService.class);
+
+        service.addStudent(student);
+
+        List<Student> students = service.getStudents();
+        System.out.println(students);
+	}
+
+}
