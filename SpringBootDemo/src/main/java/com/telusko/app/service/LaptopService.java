65b40d83e08db8e5d52fd6364148b36e11a87eaa
Natan Nascimento <natannascimento@outlook.com>
2025-11-24 10:18:48 -0300

    Moving to Spring Boot - Repository class

--- SpringBootDemo/src/main/java/com/telusko/app/service/LaptopService.java
+++ SpringBootDemo/src/main/java/com/telusko/app/service/LaptopService.java
@@ -1,12 +1,18 @@
 package com.telusko.app.service;
 
+import com.telusko.app.repository.LaptopRepository;
 import com.telusko.app.model.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LaptopService {
+
+    @Autowired
+    private LaptopRepository laptopRepository;
+
     public void addLaptop(Laptop laptop) {
-        System.out.println("Laptop added...");
+        laptopRepository.save(laptop);
     }
 
     public boolean isGoodForProgramming() {
