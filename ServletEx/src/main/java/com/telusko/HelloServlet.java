287383990719257481918c6288f1bfe5908c6ae8
Natan Nascimento <natannascimento@outlook.com>
2025-11-25 12:09:32 -0300

    Spring boot web - Running tomcat, Servlet mapping, responding to the client.

--- ServletEx/src/main/java/com/telusko/HelloServlet.java
+++ ServletEx/src/main/java/com/telusko/HelloServlet.java
@@ -6,13 +6,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void doGet   (HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("Hello from HelloServlet");
-//        res.setContentType("text/plain");
-//        res.getWriter().write("Hello from HelloServlet");
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello from HelloServlet</h1>");
     }
 }
