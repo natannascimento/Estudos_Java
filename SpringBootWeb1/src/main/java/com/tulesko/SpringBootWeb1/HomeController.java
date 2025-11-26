93dce5c5383d584fd4c5f67836205b9446283972
Natan Nascimento <natannascimento@outlook.com>
2025-11-26 11:58:21 -0300

    Spring Boot web - Add addition functionality in HomeController and create result.jsp for displaying results using RequestParam

--- SpringBootWeb1/src/main/java/com/tulesko/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/tulesko/SpringBootWeb1/HomeController.java
@@ -1,7 +1,10 @@
 package com.tulesko.SpringBootWeb1;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -9,5 +12,15 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, HttpSession session) {
+
+        int result = num1 + num2;
+
+        session.setAttribute("result", result);
+
+        return "result.jsp";
     }
 }
---
+++ SpringBootWeb1/src/main/webapp/result.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+    <head>
+        <link rel="stylesheet" text="text/css" href="style.css">
+    <body>
+
+       <!-- <h2>Result is : <%= session.getAttribute("result") %> </h2> -->
+        <h2>Result is : ${result} </h2>
+
+    </body
+</html>
