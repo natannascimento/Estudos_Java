bab15951c722a6ac7e8a78024b5cabc7570f6303
Natan Nascimento <natannascimento@outlook.com>
2025-12-01 16:06:56 -0300

    Spring Boot web - Implement JobPost model and enhance JobController with form handling

--- JobApp/src/main/java/com/telusko/JobApp/JobController.java
+++ JobApp/src/main/java/com/telusko/JobApp/JobController.java
@@ -1,18 +1,26 @@
 package com.telusko.JobApp;
 
+import com.telusko.JobApp.model.JobPost;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
 
-    @RequestMapping({"/", "home"})
+    @GetMapping({"/", "home"})
     public String home() {
         return "home";
     }
 
-    @RequestMapping("addjob")
+    @GetMapping("addjob")
     public String addJob() {
         return "addjob";
+    }
+
+    @PostMapping("handleForm")
+    public  String handleForm(JobPost jobPost) {
+        return "success";
     }
 }
