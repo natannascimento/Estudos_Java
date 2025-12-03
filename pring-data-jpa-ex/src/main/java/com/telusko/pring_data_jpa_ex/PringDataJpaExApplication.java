216d203d052ac7c91412be602cb2b39a461a547f
Natan Nascimento <natannascimento@outlook.com>
2025-12-03 09:49:15 -0300

    Spring Data JPA - JPQL Query - Refactor StudentRepo methods and update application entry point for improved data handling.

--- pring-data-jpa-ex/src/main/java/com/telusko/pring_data_jpa_ex/PringDataJpaExApplication.java
+++ pring-data-jpa-ex/src/main/java/com/telusko/pring_data_jpa_ex/PringDataJpaExApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class PringDataJpaExApplication {
 
@@ -13,23 +15,34 @@ public class PringDataJpaExApplication {
 
         StudentRepo repo = context.getBean(StudentRepo.class);
 
-        Student s1 = context.getBean(Student.class);
-        Student s2 = context.getBean(Student.class);
-        Student s3 = context.getBean(Student.class);
+//        Student s1 = context.getBean(Student.class);
+//        Student s2 = context.getBean(Student.class);
+//        Student s3 = context.getBean(Student.class);
+//
+//        s1.setRollNo(1);
+//        s1.setName("Alice");
+//        s1.setMarks(85);
+//
+//        s2.setRollNo(2);
+//        s2.setName("Bob");
+//        s2.setMarks(90);
+//
+//        s3.setRollNo(3);
+//        s3.setName("Charlie");
+//        s3.setMarks(95);
+//
+//
+//        repo.save(s2);
+//        repo.save(s3);
 
-        s1.setRollNo(1);
-        s1.setName("Alice");
-        s1.setMarks(85);
+//        Optional<Student> student = repo.findById(4);
+//
+//        System.out.println(student.orElse(new Student()));
+        //System.out.println(repo.findAll());
 
-        s2.setRollNo(2);
-        s2.setName("Bob");
-        s2.setMarks(90);
-
-        s3.setRollNo(3);
-        s3.setName("Charlie");
-        s3.setMarks(95);
-
-        repo.save(s1);
+        //repo.findByName("Alice").forEach(System.out::println);
+        //repo.findByMarks(95).forEach(System.out::println);
+        repo.findByMarksGreaterThan(86).forEach(System.out::println);
 
 	}
 
