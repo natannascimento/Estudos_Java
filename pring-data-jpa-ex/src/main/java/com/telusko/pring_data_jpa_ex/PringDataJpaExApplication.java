183e24b1828770cb55ff901dae2a3ddcba487e06
Natan Nascimento <natannascimento@outlook.com>
2025-12-03 09:54:51 -0300

    Spring Data JPA - Update application entry point to manage Student entities, including saving and deleting operations

--- pring-data-jpa-ex/src/main/java/com/telusko/pring_data_jpa_ex/PringDataJpaExApplication.java
+++ pring-data-jpa-ex/src/main/java/com/telusko/pring_data_jpa_ex/PringDataJpaExApplication.java
@@ -15,24 +15,25 @@ public class PringDataJpaExApplication {
 
         StudentRepo repo = context.getBean(StudentRepo.class);
 
-//        Student s1 = context.getBean(Student.class);
-//        Student s2 = context.getBean(Student.class);
-//        Student s3 = context.getBean(Student.class);
+        Student s1 = context.getBean(Student.class);
+        Student s2 = context.getBean(Student.class);
+        Student s3 = context.getBean(Student.class);
 //
 //        s1.setRollNo(1);
 //        s1.setName("Alice");
 //        s1.setMarks(85);
 //
-//        s2.setRollNo(2);
-//        s2.setName("Bob");
-//        s2.setMarks(90);
+        s2.setRollNo(2);
+        s2.setName("Bob");
+        s2.setMarks(65);
 //
 //        s3.setRollNo(3);
 //        s3.setName("Charlie");
 //        s3.setMarks(95);
 //
 //
-//        repo.save(s2);
+        repo.save(s2);
+        repo.delete(s2);
 //        repo.save(s3);
 
 //        Optional<Student> student = repo.findById(4);
@@ -42,7 +43,7 @@ public class PringDataJpaExApplication {
 
         //repo.findByName("Alice").forEach(System.out::println);
         //repo.findByMarks(95).forEach(System.out::println);
-        repo.findByMarksGreaterThan(86).forEach(System.out::println);
+        //repo.findByMarksGreaterThan(86).forEach(System.out::println);
 
 	}
 
